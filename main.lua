fd4dc2a4cc126f28bdf977b8d97848342898d17d
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2023-08-24 16:13:12 -0300

    add animation library

--- main.lua
+++ main.lua
@@ -1,50 +1,28 @@
 ---@diagnostic disable: lowercase-global
 love = require('love')
+local anim8 = require('lib.anim8')
 
 function love.load()
-  canvas = love.graphics.newCanvas(320, 240)
-  canvas:setFilter("nearest", "nearest")
+  love.graphics.setDefaultFilter("nearest", "nearest")
 
-  gravity = 400
+  gravity = 800
+  scale_factor = 10
 
   _G.player = {
     sprite = love.graphics.newImage("sprites/cat.png"),
-    speed = 50,
-    jump = 150,
-    dir = 1,
+    speed = 50 * scale_factor,
+    jump = 400,
+    dir = "R",
     vel = {
       x = 0,
       y = 0
     },
     pos = {
       x = 0,
-      y = 88
+      y = love.graphics.getHeight() - 20 * scale_factor
     },
     animation = {
       current = "idle",
-      prev = "idle",
-      frame = 1,
-      timer = 1,
-      ["idle"] = {
-        frames = {},
-        len = 6,
-        loop = true
-      },
-      ["run"] = {
-        frames = {},
-        len = 8,
-        loop = true
-      },
-      ["jump"] = {
-        frames = {},
-        len = 4,
-        loop = false
-      },
-      ["fall"] = {
-        frames = {},
-        len = 3,
-        loop = false
-      }
     }
   }
 
@@ -52,64 +30,51 @@ function love.load()
     local frame_w = 16
     local frame_h = 20
 
-    local offset = 1
+    self.animation.grid = anim8.newGrid(frame_w, frame_h, self.sprite:getWidth(), self.sprite:getHeight())
     -- idle
-    for i = 0, self.animation["run"].len do
-      table.insert(self.animation["idle"].frames,
-        love.graphics.newQuad(i * frame_w, 0, frame_w, frame_h, player.sprite:getWidth(),
-          player.sprite:getHeight()))
-    end
-    offset = self.animation["idle"].len
+    self.animation.idleR = anim8.newAnimation(self.animation.grid('1-6', 1), 0.1)
+    self.animation.idleL = self.animation.idleR:clone():flipH()
     -- run
-    for i = 0, self.animation["run"].len do
-      table.insert(self.animation["run"].frames,
-        love.graphics.newQuad(i * frame_w + offset * frame_w, 0, frame_w, frame_h, player.sprite:getWidth(),
-          player.sprite:getHeight()))
-    end
-    offset = offset + self.animation["run"].len
+    self.animation.runR = anim8.newAnimation(self.animation.grid('7-14', 1), 0.1)
+    self.animation.runL = self.animation.runR:clone():flipH()
     -- jump
-    for i = 0, self.animation["jump"].len do
-      table.insert(self.animation["jump"].frames,
-        love.graphics.newQuad(i * frame_w + offset * frame_w, 0, frame_w, frame_h, player.sprite:getWidth(),
-          player.sprite:getHeight()))
-    end
-    offset = offset + self.animation["jump"].len
+    self.animation.jumpR = anim8.newAnimation(self.animation.grid('15-19', 1), 0.1, 'pauseAtEnd')
+    self.animation.jumpL = self.animation.jumpR:clone():flipH()
     -- fall
-    for i = 0, self.animation["fall"].len do
-      table.insert(self.animation["fall"].frames,
-        love.graphics.newQuad(i * frame_w + offset * frame_w, 0, frame_w, frame_h, player.sprite:getWidth(),
-          player.sprite:getHeight()))
-    end
+    self.animation.fallR = anim8.newAnimation(self.animation.grid('19-21', 1), 0.1, 'pauseAtEnd')
+    self.animation.fallL = self.animation.fallR:clone():flipH()
+  end
+
+  function player:getAnim()
+    return player.animation[player.animation.current .. player.dir]
   end
 
   function player:change_anim(animation, dir)
-    self.animation.prev = self.animation.current
     self.animation.current = animation
 
     if dir ~= nil then
       self.dir = dir
     end
-    if self.animation.prev ~= animation then -- reset animation
-      self.animation.frame = 1
-      self.animation.timer = 1
-    end
   end
 
   function player:update(dt)
+    ---@diagnostic disable-next-line: undefined-field
+    player:getAnim():update(dt)
+
     if self.vel.x == 0 and self.vel.y == 0 then
-      player:change_anim("idle")
+      player.animation.current = "idle"
     end
 
     player.vel.x = 0
 
     if love.keyboard.isDown('d') then
       player.vel.x = self.speed
-      self.dir = 1
+      self.dir = "R"
     end
 
     if love.keyboard.isDown('a') then
       player.vel.x = -self.speed
-      self.dir = -1
+      self.dir = "L"
     end
 
     if love.keyboard.isDown('space') then
@@ -120,16 +85,16 @@ function love.load()
 
     if self.vel.y == 0 then
       if self.vel.x > 0 then
-        player:change_anim("run")
+        player.animation.current = "run"
       elseif self.vel.x < 0 then
-        player:change_anim("run")
+        player.animation.current = "run"
       end
     end
 
     if self.vel.y < 0 then
-      player:change_anim("jump")
+      player.animation.current = "jump"
     elseif self.vel.y > 0 then
-      player:change_anim("fall")
+      player.animation.current = "fall"
     end
 
     if self.vel.x ~= 0 then
@@ -141,41 +106,15 @@ function love.load()
       self.vel.y = self.vel.y + gravity * dt
     end
     -- hit ground
-    if self.pos.y > 88 then
+    if self.pos.y > love.graphics.getHeight() - 20 * scale_factor then
       self.vel.y = 0
-      self.pos.y = 88
-    end
-  end
-
-  function player:animate(dt)
-    local current_animation = self.animation[self.animation.current]
-    self.animation.timer = self.animation.timer + dt
-
-    if self.animation.timer > 0.2 then
-      self.animation.timer = 0.1
-      self.animation.frame = self.animation.frame + 1
-
-      if current_animation ~= nil and self.animation.frame > current_animation.len then
-        if current_animation.loop then
-          self.animation.frame = 1
-        else
-          self.animation.frame = current_animation.len
-        end
-      end
+      self.pos.y = love.graphics.getHeight() - 20 * scale_factor
     end
   end
 
   function player:draw()
-    local current_animation = self.animation[self.animation.current]
-    if current_animation ~= nil then
-      if self.dir < 0 then
-        love.graphics.draw(self.sprite, current_animation.frames[self.animation.frame],
-          self.pos.x, self.pos.y, 0, -1, 1, 16, 0)
-      else
-        love.graphics.draw(self.sprite, current_animation.frames[self.animation.frame],
-          self.pos.x, self.pos.y)
-      end
-    end
+    ---@diagnostic disable-next-line: undefined-field
+    player:getAnim():draw(self.sprite, self.pos.x, self.pos.y, nil, scale_factor, scale_factor)
   end
 
   player:load_sprite()
@@ -189,22 +128,15 @@ end
 
 function love.update(dt)
   player:update(dt)
-  player:animate(dt)
 end
 
 function love.draw()
   love.graphics.push()
   love.graphics.scale(1.5)
   love.graphics.setColor(1, 1, 1)
-  love.graphics.print(string.format("%s %s", player.animation.current, player.animation[player.animation.current].len), 0,
-    0)
+  love.graphics.print(string.format("%s", player.animation.current), 0, 0)
   love.graphics.print(string.format("vel.x %d, vel.y %d", player.vel.x, player.vel.y), 0, 20)
   love.graphics.pop()
 
-  love.graphics.setCanvas(canvas)
-  love.graphics.clear()
   player:draw()
-
-  love.graphics.setCanvas()
-  love.graphics.draw(canvas, 0, 0, 0, 10, 10)
 end
