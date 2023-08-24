65ea1cf07ef06161a92c234231657bfbffa95a5d
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2023-08-23 22:54:34 -0300

    init game

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+  t.version = "11.3"
+
+  t.window.title = "Furball"
+  t.window.icon = "sprites/furball.png"
+  t.window.width = 1920
+  t.window.height = 1080
+  t.window.resizable = false
+  -- t.window.fullscreen = true
+  t.window.vsync = 1
+end
